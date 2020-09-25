9e31bf7a47a9e5efb3017a7dd5ed78d08732ed33
Cory <cjm48d@mail.umsl.edu>
2020-09-25 17:34:53 -0500

    Made 36 card backs appear in a 6 x 6 grid.

--- Matching Card Game/Game Play/GamePlayViewController.swift
+++ Matching Card Game/Game Play/GamePlayViewController.swift
@@ -2,8 +2,26 @@ import UIKit
 
 class GamePlayViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
+}
+
+extension GamePlayViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        36
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Card Cell", for: indexPath)
+        cell.layer.cornerRadius = cell.frame.size.width / 6
+        cell.clipsToBounds = true
+        return cell
+    }
+}
+
+extension GamePlayViewController: UICollectionViewDelegate {
+    
 }
